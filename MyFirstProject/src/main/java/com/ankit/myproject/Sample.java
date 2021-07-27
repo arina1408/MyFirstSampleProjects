2f19d3fde327cb6fb43e6da4ff93ddb6ca8eb52c
Admin <Admin@PC1408-ANKIT>
2021-07-27 18:26:08 +0530

    Commit 2

--- MyFirstProject/src/main/java/com/ankit/myproject/Sample.java
+++ MyFirstProject/src/main/java/com/ankit/myproject/Sample.java
@@ -1,11 +1,58 @@
 package com.ankit.myproject;
 
-public class Sample {
+//Immutable Class
+class Engine{
+	int speed;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.println("Ankit and Rina Chaurasia");
-     
+	public Engine(int speed) {
+		this.speed = speed;
+	}
+	
+	
+}
+public final class Sample
+
+{
+	
+	private final int id;
+	private final String name;
+	private final Engine engine;
+	
+	
+	
+	public Sample(int id, String name, Engine e) {
+		this.id = id;
+		this.name = name; 
+		Engine newEngine = new Engine(e.speed);
+		this.engine = newEngine;
+
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public Engine getEngine() {
+		 return new Engine(this.engine.speed);
 	}
 
-}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+	public static void main(String []args)
+	{   
+		Engine e = new Engine(50);
+		Sample s= new Sample(1,"ankit",e);
+		System.out.println(s.engine.speed);
+		e.speed=70;
+		System.out.println(s.engine.speed);
+		
+	}
+	
+	
+	}
+
+
